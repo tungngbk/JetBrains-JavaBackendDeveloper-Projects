d6ac6206df74dff33e57e52a6704315911ae82fd
zuankid <nhokute.1303@gmail.com>
2022-08-01 10:33:38 +0700

    Add initial challenging project Code Sharing Platform

---
+++ Challenging/Code Sharing Platform/code-sharing-platform/src/test/java/com/zuankid/codesharingplatform/CodeSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuankid.codesharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
